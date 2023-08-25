675d77fcaae4beb48a5bddfc9898fa24a356316e
Артём Корнилов <temka.kornilov@mail.ru>
2023-08-25 12:57:05 +0300

    Задание урока 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
+const Message = (props) => {
+  return(
+    <h1 className='red-text'>{props.text}</h1>
+  );
+}
+
 const App = () => {
+  const text = "Передаваемый пропсом текст";
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Message text={text} />
       </header>
     </div>
   );
